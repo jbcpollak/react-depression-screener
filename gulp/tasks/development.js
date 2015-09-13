db7325a1294caf1aca79619e436204337369df0c
Joshua Chaitin-Pollak <josh@offthehill.org>
2015-09-12 23:14:20 -0400

    incremental tweaks

--- gulp/tasks/development.js
+++ gulp/tasks/development.js
@@ -5,8 +5,6 @@ var runSequence = require('run-sequence');
 
 gulp.task('dev', ['clean'], function(cb) {
 
-  cb = cb || function() {};
-
   global.isProd = false;
 
   runSequence(['styles', 'images', 'fonts', 'views', 'browserify'], 'watch', cb);
--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -5,8 +5,6 @@ var runSequence = require('run-sequence');
 
 gulp.task('prod', ['clean'], function(cb) {
 
-  cb = cb || function() {};
-
   global.isProd = true;
 
   runSequence(['styles', 'images', 'fonts', 'views', 'browserify'], 'gzip', cb);
